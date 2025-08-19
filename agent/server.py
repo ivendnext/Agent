e9d4d56c9aeaf6a111ffe862b26dadf6080ce375
phot0n <ritwikpuri5678@gmail.com>
2025-08-19 11:15:47 +0530

    fix: create apps dir for devbox mount

--- agent/server.py
+++ agent/server.py
@@ -53,6 +53,8 @@ class Server(Base):
         bench_directory = os.path.join(self.benches_directory, name)
         os.mkdir(bench_directory)
         directories = ["logs", "sites", "config"]
+        if config.get("for_devbox", None):
+            directories.append("apps")
         for directory in directories:
             os.mkdir(os.path.join(bench_directory, directory))
 
